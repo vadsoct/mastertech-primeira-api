4c51545626c93a116395eb40dffd4398b7b60d26
Alberto Anderick Jr <beto@anderick.com>
2017-07-06 02:04:07 -0300

    basic auth

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var expressMongoDb = require('express-mongo-db');
 
+var basicAuth = require('basic-auth');
+
 // inicializa o express
 var app = express();
 
@@ -16,12 +18,31 @@ app.listen(3000, function() {
   console.log('Acesse o servidor http://localhost:3000');
 });
 
+var auth = function (req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.sendStatus(401);
+  };
+
+  var user = basicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+
+  if (user.name === 'foo' && user.pass === 'bar') {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
+
 // importa controllers
 var alunosController = require('./controllers/alunos.js');
 var professoresController = require('./controllers/professores.js');
 
 // cria enpoints para funcoes de controllers
-app.get('/alunos', alunosController.listar);
+app.get('/alunos', auth, alunosController.listar);
 app.post('/alunos', alunosController.criar);
 app.put('/alunos/:id', alunosController.atualizar);
 app.get('/alunos/:id', alunosController.recuperar);
